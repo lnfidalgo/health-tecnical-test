81d5b6e9a16137d6c48a5be9f4e77b63ccc46f8f
lnfidalgo <lucasfidalgo07@gmail.com>
2026-02-27 13:31:00 -0300

    FIX: metadata desciption

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Agenda",
-  description: "Agenda estilo Google Calendar (mock)",
+  description: "Agendamentos pessoais",
 };
 
 export default function RootLayout({
